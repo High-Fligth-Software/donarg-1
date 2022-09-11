b886fa5e28320b698d8e9b63fcc32233bdf1020c
RaguebRogerio <70453161+RaguebRogerio@users.noreply.github.com>
2022-09-11 14:46:41 -0300

    Carga de imagenes el primer paso esta listo

--- src/Page/CrearUsuarioImagen/index.js
+++ src/Page/CrearUsuarioImagen/index.js
@@ -1,6 +1,5 @@
 import React ,{useState} from 'react'
-import { Grid, Typography, Button, TextField, FilledInput  } from '@mui/material';
-import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import { Grid, Typography, Button, TextField, FilledInput, Avatar} from '@mui/material';
 import { useNavigate  } from 'react-router-dom'
 import { ModalComponent } from '../../Components/Modal';
 import donarg from '../../imagenes/donARG.PNG'
@@ -8,6 +7,32 @@ import donarg from '../../imagenes/donARG.PNG'
 export const CrearUsuarioImagen = () => {
     const navigate = useNavigate();
     const [modalCreacionCorrecta, setModalCreacionCorrecta] = useState(false)
+    const [perfilImage, setPerfilImage] = useState("");
+    async function onButtonClicked(){
+        let contentElement = document.getElementById("content");
+        let files = await selectFile("image/*", true);
+        const file =  files.map(file => `"${URL.createObjectURL(file)}"`);
+        setPerfilImage(file[0])
+        //Tambien se puede usar un valor porcentual que se adapte a la imagen
+        contentElement.innerHTML = files.map(file => `<img src="${URL.createObjectURL(file)}" style="width: 200px; height: 200px; border-radius:160px; border:3px solid #666;">`).join('');
+    }
+    function selectFile (contentType, multiple){
+        return new Promise(resolve => {
+            let input = document.createElement('input');
+            input.type = 'file';
+            input.multiple = multiple;
+            input.accept = contentType;
+    
+            input.onchange = _ => {
+                let files = Array.from(input.files);
+                if (multiple)
+                    resolve(files);
+                else
+                    resolve(files[0]);
+            };
+            input.click();
+        });
+    }
   return (
     <>
         <div class="LoginForm">
@@ -37,7 +62,15 @@ export const CrearUsuarioImagen = () => {
                     </Typography>
                 </Grid>
                 <Grid item xs={12} md={12} lg={12}>
-                    <FilledInput type="file"></FilledInput>
+                    {perfilImage === '' ? 
+                        <Avatar
+                        sx={{ width: 100, height: 100,  cursor:"pointer"}}
+                        onClick={()=>{onButtonClicked()}}
+                        />:null
+                    }
+                </Grid>
+                <Grid item xs={12} md={12} lg={12}>
+                    <div id="content" style={{cursor:"pointer", display: "flex", justifyContent: "center"}} onClick={()=>{onButtonClicked()}}></div>
                 </Grid>
             </Grid>
             <div style={{marginTop:"4%"}}>
