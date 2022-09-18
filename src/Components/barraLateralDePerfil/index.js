b472acc4f326a057ecdaf5ac98b1e6a71ed6b7a0
RaguebRogerio <70453161+RaguebRogerio@users.noreply.github.com>
2022-09-18 17:26:47 -0300

    Lista la barra lateral de perfil

---
+++ src/Components/barraLateralDePerfil/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import {Grid,Typography, Container, Divider } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
+import TurnedInIcon from '@mui/icons-material/TurnedIn';
+import donarg from '../../imagenes/donARG.PNG'
+import './barraLateralDePerfil.css'
+
+export const BarraLateralDePerfil = () => {
+    const userData = require('../../Mocks/user.json')
+    console.log(userData)
+  return (
+    <Container style={{backgroundColor: 'white', borderRadius:"25px"}}>
+        <Grid container alignItems="center" justifyContent="flex-start" direction="column" spacing={0.5}>
+                <Grid item xs={12} md={12} lg={12}>
+                    <img alt="foto de perfil" src={userData.data.fotoDePerfil} style={{width: "80px", height: "80px", borderRadius: "160px",}}/>
+                 </Grid>
+                <Grid item xs={12} md={12} lg={12}>
+                    <Typography variant="h6" gutterBottom component="div">
+                        <b>{userData.data.userName}</b>
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} md={12} lg={12} style={{textAlign: "center",}}>
+                    <Typography variant="h7" gutterBottom component="div" style={{color:"grey"}}>
+                        {userData.data.descripcion}
+                    </Typography>
+                </Grid>              
+        </Grid>
+        <Divider/>
+        <Grid className="presionablesLaterales" container direction="row"  justifyContent="flex-start" onClick={()=>{console.log("Mostrar perfil")}} spacing={1}>
+                        <Grid item>
+                            <PersonIcon sx={{ fontSize: 20 }} />
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="h7" gutterBottom component="div">
+                                Mira tu perfil
+                            </Typography>
+                        </Grid>
+        </Grid>
+        <Grid className="presionablesLaterales" container direction="row"  justifyContent="flex-start" onClick={()=>{console.log("Mostrar perfil")}} spacing={1}>
+                        <Grid item>
+                            <VolunteerActivismIcon sx={{ fontSize: 20 }} />
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="h7" gutterBottom component="div">
+                                Ayudanos a crecer
+                            </Typography>
+                        </Grid>
+        </Grid>
+        <Grid className="presionablesLaterales" container direction="row"  justifyContent="flex-start" onClick={()=>{console.log("Mostrar perfil")}} spacing={1}>
+                        <Grid item>
+                            <TurnedInIcon sx={{ fontSize: 20 }} />
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="h7" gutterBottom component="div">
+                                Publicaciones guardadas
+                            </Typography>
+                        </Grid>
+        </Grid>
+        <Divider/>
+        <Grid container direction="row"  justifyContent="center" style={{marginTop:"1%"}}>
+            <Grid item>
+                <img src={donarg} alt="hola" width="200px" height="50px"/>
+            </Grid>
+        </Grid>
+    </Container>
+
+  )
+}
--- src/Page/Inicio/index.js
+++ src/Page/Inicio/index.js
@@ -1,21 +1,22 @@
 import React from 'react'
-import {Grid} from '@mui/material';
-
+import {Grid,Container} from '@mui/material';
 import { NavBar } from '../../Components/NavBar';
-
+import { BarraLateralDePerfil } from '../../Components/barraLateralDePerfil';
 export const Inicio = () => {
     return (
         <>
             <NavBar/>
-                <Grid container  direction="row">
-                    <Grid item xs={3} md={3} lg={3}>
-                        <item><h1>Holaaaaaaaaaaaa</h1></item>
+                <Grid container alignItems="flex-start" justifyContent="center"  direction="row" style={{marginTop:"0.1%"}} spacing={1}>
+                    <Grid item xs={2} md={2} lg={2}>
+                            <BarraLateralDePerfil/>
                     </Grid>
                     <Grid item xs={6} md={6} lg={6}>
-                        <item><h1>Holaaaaaaaaaaaa</h1></item>
+                        <Container style={{backgroundColor: 'white', borderRadius:"25px"}}>
+                            <h1>Holaaaaaaaaaaaa</h1>
+                        </Container>                            
                     </Grid>
-                    <Grid item xs={3} md={3} lg={3}>
-                        <item><h1>Holaaaaaaaaaaaa</h1></item>
+                    <Grid item xs={2} md={2} lg={2}>
+                    <Container style={{backgroundColor: 'white', borderRadius:"25px"}}><h1>Holaaaaaaaaaaaa</h1></Container>
                     </Grid>
                 </Grid>                  
         </>
